24adfcb50d23fdc71ea7238383fdbc2d4b35d811
EmmaOverbeek <emmaroverbeek@gmail.com>
2024-02-01 17:25:48 -0500

    Updated KrakenBot with all changes we've made to the NeoBot

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -29,7 +29,7 @@ public class RobotMap {
 
     }
 
-        /**
+    /**
      * All pilot controller constants
      */
     public static class PilotControllerConstants {
@@ -54,5 +54,59 @@ public class RobotMap {
          * Initial value matches 2022 robot value of 2.25, but can be adjusted as needed
          */
         public static final double ACCEL_SLEW_RATE = 1.6;
+    }
+
+    /**
+     * All Indexer constants
+     */
+    public static class IndexerConstants {
+
+        /**
+         * CAN ID of the index motor
+         */
+        public static final int INDEXER_CAN_ID = 10;
+
+        /**
+         * DIO port of the index sensor
+         */
+        public static final int SENSOR_PORT = 0;
+
+        /**
+         * Speed set to index motor for loading note from intake
+         */
+        public static final double LOAD_SPEED = 0.4;
+
+        /**
+         * Speed set to index motor for feeding note into launcher
+         */
+        public static final double FEED_SPEED = 0.6;
+    }
+
+    /**
+     * All Intake constants
+     */
+    public static class IntakeConstants {
+
+        /**
+         * CAN ID of the intake motor
+         */
+        public static final int INTAKE_CAN_ID = 19;
+
+    }
+
+    /**
+     * All Launcher constants
+     */
+    public static class LauncherConstants {
+
+        /**
+         * CAN ID of the left launcher motor
+         */
+        public static final int LEFT_LAUNCHER_CAN_ID = 17;
+
+        /**
+         * CAN ID of the right launcher motor
+         */
+        public static final int RIGHT_LAUNCHER_CAN_ID = 18;
     }
 }
